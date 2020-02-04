51a79a2f4e7157da94e1bd6d43e72c9d35e4a1be
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-04 11:53:42 -0500

    Adds an import statement for 'load_dotenv' from the 'dotenv' module

--- scenic_project/wsgi.py
+++ scenic_project/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scenic_project.settings')
