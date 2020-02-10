1febb14b62381f8cd72e510514ee30f4dfeda298
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-10 12:58:22 -0500

    Enables the Admin user to check/uncheck the 'Is Published' attribute for each listing from the full list of listings

--- listings/admin.py
+++ listings/admin.py
@@ -5,5 +5,6 @@ class ListingAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
   list_display_links = ('id', 'title')
   list_filter = ('realtor', 'price', 'list_date')
+  list_editable = ('is_published',)
 
 admin.site.register(Listing, ListingAdmin)
