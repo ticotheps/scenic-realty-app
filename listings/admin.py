65e4a597c64d8a5c443382af2e91e8ae5ece6489
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-10 12:49:59 -0500

    Enables Admin user to filter listings page results by: realtor, price, and list_date

--- listings/admin.py
+++ listings/admin.py
@@ -4,5 +4,6 @@ from .models import Listing
 class ListingAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
   list_display_links = ('id', 'title')
+  list_filter = ('realtor', 'price', 'list_date')
 
 admin.site.register(Listing, ListingAdmin)
